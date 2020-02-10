8db7ba05cbe47926dfdc359894831285a7c5af18
elhombre <elhombre.dev@gmail.com>
2020-02-10 17:30:58 +0500

    Add config files

---
+++ README.md
@@ -0,0 +1,7 @@
+# Aperos datanode
+
+The data node allows you to build convenient hierarchical structures
+with tracking of change events.
+
+The development branch can contain experimental code that is under
+active development and should not be used for production environments.
---
+++ rollup.config.js
@@ -0,0 +1,47 @@
+import path from 'path'
+import pluginTypescript from 'rollup-plugin-typescript2'
+import pluginDts from 'rollup-plugin-dts'
+import { terser as pluginTerser } from 'rollup-plugin-terser'
+import pkg from './package.json'
+
+const external = [
+  ...Object.keys(process.binding('natives')),
+  ...Object.keys(pkg.dependencies || {})
+]
+
+const targetDir = 'dist'
+
+const plugins = [
+  pluginTypescript({
+    useTsconfigDeclarationDir: true,
+    verbosity: 1
+  })
+]
+
+if (process.env.BUILD === 'production') {
+  plugins.push(
+    pluginTerser({
+      output: { comments: false },
+      toplevel: true
+    })
+  )
+}
+
+export default [
+  {
+    input: 'lib/index.ts',
+    external,
+    output: {
+      file: path.join(targetDir, 'index.js'),
+      format: 'cjs',
+      name: 'index',
+      sourcemap: true
+    },
+    plugins
+  },
+  {
+    input: './build/index.d.ts',
+    output: [{ file: path.join(targetDir, 'index.d.ts'), format: 'es' }],
+    plugins: [pluginDts({ verbose: true })]
+  }
+]
