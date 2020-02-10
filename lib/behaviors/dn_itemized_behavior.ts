75ff35d2aea341380d3bccb7b9d1d01e2afdbee0
elhombre <elhombre.dev@gmail.com>
2020-02-11 01:52:04 +0500

    Add basic DnItemizedBehavior

---
+++ lib/behaviors/dn_itemized_behavior.ts
@@ -0,0 +1,115 @@
+import { IBitFlags } from '@aperos/ts-goodies'
+import { ClassName } from '@aperos/essentials'
+import { IDataNode, IDataNodeEvent } from '../data_node'
+import { IDataNodeLink } from '../data_node_link'
+import {
+  DataNodeBehavior,
+  DataNodeBehaviorFlags,
+  IDataNodeBehavior,
+  IDataNodeBehaviorOpts
+} from './data_node_behavior'
+
+export interface IDnItemizedBehavior extends IDataNodeBehavior {
+  readonly dnIndex: IDataNode
+  readonly dnItems: IDataNode
+}
+
+export interface IDnItemizedBehaviorOpts extends IDataNodeBehaviorOpts {
+  allowMultiselect?: boolean
+  roundRobin?: boolean
+}
+
+export type DnItemizedBehaviorFlags = 'AllowMultiselect' | 'RoundRobin' | DataNodeBehaviorFlags
+
+type EssentialChildren = 'dnIndex' | 'dnItems'
+
+@ClassName('DnItemizedBehavior')
+export class DnItemizedBehavior extends DataNodeBehavior implements IDnItemizedBehavior {
+  private static readonly selectionsMap = new WeakMap<IDataNode, IDataNode>()
+  readonly flags!: IBitFlags<DnItemizedBehaviorFlags>
+  protected readonly essentials: Partial<Record<EssentialChildren, IDataNode>> = {}
+
+  get allowMultiselect() {
+    return this.flags.isSet('AllowMultiselect')
+  }
+
+  get dnIndex() {
+    return this.essentials['dnIndex']!
+  }
+
+  get dnItems() {
+    return this.essentials['dnItems']!
+  }
+
+  get roundRobin() {
+    return this.flags.isSet('RoundRobin')
+  }
+
+  protected readonly selectionChangeListener = (event: IDataNodeEvent) => {
+    const s = event.node
+    const p = s.parent!
+    this.applyUpdates(() => {
+      if (s.value) {
+        this.selectItem(p)
+      } else {
+        this.unselectItem(p)
+      }
+    })
+  }
+
+  protected selectItem(item: IDataNode) {}
+
+  protected unselectItem(item: IDataNode) {}
+
+  protected initBehavior(opts: IDnItemizedBehaviorOpts) {
+    const { dataNode: dn, dnIndex, dnItems, flags, essentials: xs, selectionsMap: sm } = this
+    flags.setFlag('AllowMultiselect', opts.allowMultiselect)
+    flags.setFlag('RoundRobin', opts.roundRobin)
+
+    xs.dnIndex = dn.makePath('index')!
+    xs.dnItems = dn.makePath('items')!
+
+    this.applyUpdates(() => {
+      dnIndex.enumChildren(c => {
+        this.initItem(c)
+      })
+    })
+
+    dnIndex.on('change', () => {
+      this.updateIndex(dnIndex.getInt())
+    })
+
+    dnItems
+      .on('addChild', event => {
+        const c = event.child!
+        this.initItem(c)
+        const s = sm.get(c)!
+        s.on('change', this.selectionChangeListener)
+      })
+      .on('removeChild', event => {
+        const c = event.child!
+        const s = sm.get(c)!
+        sm.delete(c)
+        if (c.parent === dnItems && s.value) {
+          this.updateIndex(dnItems.childCount > 0 ? Math.max(0, c.childIndex - 1) : -1)
+        }
+        s.off('change', this.selectionChangeListener)
+      })
+  }
+
+  protected initItem(item: IDataNode, selected = false) {
+    const { selectionsMap: sm } = this
+    const s = item.makePath('selected')!
+    if (!sm.has(item)) {
+      sm.set(item, s)
+    }
+    s.value = selected
+    s.on('change', this.selectionChangeListener)
+  }
+
+  protected updateIndex(index: number) {}
+
+  private get selectionsMap() {
+    return DnItemizedBehavior.selectionsMap
+  }
+}
